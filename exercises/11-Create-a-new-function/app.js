9528da3f0238bfb0ab5392d429581c7bf4cb896e
flopezcardozo <flopezcardozo@gmail.com>
2022-01-05 00:26:06 +0000

    Ejercicio 13 resuelto

--- exercises/11-Create-a-new-function/app.js
+++ exercises/11-Create-a-new-function/app.js
@@ -1,3 +1,7 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
-// Declare your function here
+function generateRandom (min, max){
+    return Math.floor(Math.random()* (max-min)+1) ;
+ }
+
+ console.log(generateRandom(0,9));
